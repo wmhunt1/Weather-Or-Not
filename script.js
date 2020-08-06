4b737fb2c66968b818955c36a606464a0227485d
Matt Hunt <wmatthunt@gmail.com>
2020-08-06 19:43:17 -0400

    api key works

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 function displayWeatherInfo() {
 
     var city = "London,uk"
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ec47932de753d8f1e0150dd93dbbd543";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY";
+   // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7758a0a2e7e596b9ee3eb0ba1a3e2444";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7758a0a2e7e596b9ee3eb0ba1a3e2444";
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
@@ -12,6 +12,7 @@ function displayWeatherInfo() {
         method: "GET"
     }).then(function (response) {
         console.log(response)
+        console.log(response.name + ",")
     });
 
 }
