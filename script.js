795186b46c3593e18400a6301c58824dc51878a1
Matt Hunt <wmatthunt@gmail.com>
2020-08-06 20:37:46 -0400

    formatting

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ $(document).ready(function () {
 function displayWeatherInfo() {
 
     var city = "London,uk"
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7758a0a2e7e596b9ee3eb0ba1a3e2444";
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=7758a0a2e7e596b9ee3eb0ba1a3e2444";
-
-    // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
