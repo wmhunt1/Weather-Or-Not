c6ef10cdbbed7f47c92ad0753632317e13fd3f07
Matt Hunt <wmatthunt@gmail.com>
2020-08-06 20:37:01 -0400

    will finish uvi later

--- script.js
+++ script.js
@@ -13,15 +13,26 @@ function displayWeatherInfo() {
     }).then(function (response) {
         console.log(response)
         console.log(response.city.name + ", " + response.city.country)
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < 5; i++) {
             console.log("Day" + (i + 1))
-            console.log(response.list[i].weather[0].main)
+            console.log("Weather: " + response.list[i].weather[0].main)
             console.log((response.list[i].main.temp) + " Kelvin")
             console.log((response.list[i].main.humidity) + "% humidity")
             console.log((response.list[i].wind.speed) + " kmh wind speed")
-            console.log(response.city.coord.lat + "," + response.city.coord.lon)
+            // console.log(response.city.coord.lat + "," + response.city.coord.lon)
         }
     });
-
+    // var lat = response.city.coord.lat;
+    // var lon = response.city.coord.lon;
+    // var queryUVI = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=7758a0a2e7e596b9ee3eb0ba1a3e2444";
+    // $.ajax({
+    //     url: queryUVI,
+    //     method: "GET"
+    // }).then(function (UVI) {
+    //     console.log(UVI)
+    //     for (var i = 0; i < 5; i++) {
+    //     console.log(UVI[i].value + " UVI")
+    //     }
+    // });
 }
 displayWeatherInfo()
